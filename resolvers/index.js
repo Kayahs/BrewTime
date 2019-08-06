791834a463505a8bb8575aae2de84582508e2c02
Kayahs <akshaykmanchanda@gmail.com>
2019-08-06 09:52:30 -0400

    Added resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,7 +1,9 @@
 const queryResolvers = require("./query/queryResolvers")
 const mutationResolvers = require("./mutation/mutationResolvers")
+const breweryResolvers = require("./brewery/breweryResolvers")
 
 module.exports = {
   ...queryResolvers,
-  ...mutationResolvers
+  ...mutationResolvers,
+  ...breweryResolvers
 }
