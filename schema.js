95f4d8c83eca77335a9948f85f644961aba9ed1c
Kayahs <akshaykmanchanda@gmail.com>
2019-08-06 09:44:13 -0400

    Adjusted schema to have all the types

--- schema.js
+++ schema.js
@@ -2,10 +2,55 @@ const { gql } = require("apollo-server-express")
 
 module.exports = gql`
   type Query {
-    test: String!
+    getBreweries: [Brewery]!
   }
 
   type Mutation {
     test: String!
+  }
+
+  type Brewery {
+    id: ID!
+    name: String!
+    description: String!
+    locations: [Location]!
+    bookings: [Booking]!
+    images: [Image]!
+    products: [Product]!
+  }
+
+  type Location {
+    id: ID!
+    address: String!
+    description: String
+    type: String!
+    brewery: Brewery!
+  }
+
+  type Booking {
+    id: ID!
+    title: String!
+    description: String!
+    location: Location!
+    guide: String!
+    time: String!
+    brewery: Brewery!
+  }
+
+  type Image {
+    id: ID!
+    uri: String!
+    description: String!
+    width: Int!
+    height: Int!
+  }
+
+  type Product {
+    id: ID!
+    name: String!
+    description: String!
+    image: String!
+    price: Int!
+    brewery: Brewery
   }
 `
