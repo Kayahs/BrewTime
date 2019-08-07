5a4457a5cba4be30717122e9004dcdac4663f8d1
Kayahs <akshaykmanchanda@gmail.com>
2019-08-07 13:46:34 -0400

    Add some resolvers and adjust some for new structure

--- resolvers/location/locationResolvers.js
+++ resolvers/location/locationResolvers.js
@@ -1,14 +1,3 @@
 module.exports = {
-  Location: {
-    async brewery(parent, args, { postgres }, info) {
-      const getBreweryQuery = {
-        text: "SELECT * FROM brewtime.breweries",
-        values: [parent.brewery_id]
-      }
-
-      const getBreweryResult = await postgres.query(getBreweryQuery)
-
-      return getBreweryResult.rows[0]
-    }
-  }
+  Location: {}
 }
--- resolvers/query/queryResolvers.js
+++ resolvers/query/queryResolvers.js
@@ -8,6 +8,16 @@ module.exports = {
       const getBreweriesResult = await postgres.query(getBreweriesQuery)
 
       return getBreweriesResult.rows
+    },
+    async getBreweryInfo(parent, { input }, { postgres }, info) {
+      const getBreweryQuery = {
+        text: "SELECT * FROM brewtime.breweries WHERE id = $1",
+        values: [input.brewery_id]
+      }
+
+      const getBreweryResult = await postgres.query(getBreweryQuery)
+
+      return getBreweryResult.rows[0]
     }
   }
 }
